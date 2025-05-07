d43b5c5c90381c57d1493d1fd6c1391f6a241779
waleedzahid281 <bscs2312182@szabist.pk>
2025-05-07 13:15:33 +0000

    GET METHOD

--- Api/index.js
+++ Api/index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const app=express();
 app.use(cors());
 app.use(express.json());
+
 app.get('/',(req,res)=>{
     try{
         res.json(`welcome to hr api`);
@@ -22,8 +23,142 @@ app.get('/region',async(req,res)=>{
     }
 });
 
-const PORT=process.env.PORT;
+
+
+app.get('/employee',async(req,res)=>{
+    try{
+        const result=await pool.query('Select * from employees');
+        res.json(result.rows);
+
+    }catch(err){
+        res.status(500).json({Error:err.message})
+
+    }
+});
+
+
+app.get('/job',async(req,res)=>{
+    try{
+        const result=await pool.query('Select * from jobs');
+        res.json(result.rows);
+
+    }catch(err){
+        res.status(500).json({Error:err.message})
+
+    }
+});
+
+
+
+app.get('/department',async(req,res)=>{
+    try{
+        const result=await pool.query('Select * from departments');
+        res.json(result.rows);
+
+    }catch(err){
+        res.status(500).json({Error:err.message})
+
+    }
+});
+
+
+app.get('/location',async(req,res)=>{
+    try{
+        const result=await pool.query('Select * from locations');
+        res.json(result.rows);
+
+    }catch(err){
+        res.status(500).json({Error:err.message})
+
+    }
+});
+
+
+app.get('/totalemp',async(req,res)=>{
+    try{
+        const result=await pool.query('Select count(*) from employees');
+        res.json(result.rows);
+
+    }catch(err){
+        res.status(500).json({Error:err.message})
+
+    }
+});
+
+
+app.get('/countrycount',async(req,res)=>{
+    try{
+        const result=await pool.query('Select count(*) from countries');
+        res.json(result.rows);
+
+    }catch(err){
+        res.status(500).json({Error:err.message})
+
+    }
+});
+
+
+app.get('/totaljobs',async(req,res)=>{
+    try{
+        const result=await pool.query('Select count(*) from jobs');
+        res.json(result.rows);
+
+    }catch(err){
+        res.status(500).json({Error:err.message})
+
+    }
+});
+
+
+app.get('/totaljobjistory',async(req,res)=>{
+    try{
+        const result=await pool.query('Select count(*) from jobhistory');
+        res.json(result.rows);
+
+    }catch(err){
+        res.status(500).json({Error:err.message})
+
+    }
+});
+
+
+
+app.get('/totallocation',async(req,res)=>{
+    try{
+        const result=await pool.query('Select count(*) as location_count from locations');
+        res.json(result.rows);
+
+    }catch(err){
+        res.status(500).json({Error:err.message})
+
+    }
+});
+
+
+app.get('/totalregion',async(req,res)=>{
+    try{
+        const result=await pool.query('Select count(*) from regions');
+        res.json(result.rows);
+
+    }catch(err){
+        res.status(500).json({Error:err.message})
+
+    }
+});
+
+app.get('/country',async(req,res)=>{
+    try{
+        const result=await pool.query('Select * from countries');
+        res.json(result.rows);
+
+    }catch(err){
+        res.status(500).json({Error:err.message})
+
+    }
+});
+
+
+const PORT=process.env.port;
 app.listen(PORT,()=>{
     console.log(`Connected sucessfully.... on port ${PORT}`)
 });
-
